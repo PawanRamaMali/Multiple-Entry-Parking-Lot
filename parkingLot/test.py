6cdac22911b77422d8ce600e138de01249f4e932
Pawan Rama Mali <prm@outlook.in>
2021-12-26 14:49:03 +0530

    Create test.py

---
+++ parkingLot/test.py
@@ -0,0 +1,32 @@
+""""
+Text Wraping
+
+"""
+
+# input_text_para = "The CK-12 Foundation"
+# width_of_window = 100
+#
+# all_words = iter(input_text_para.split())
+#
+# lines = []
+# current = next(all_words)
+#
+#
+# for word in all_words:
+#     if len(current) + 1 + len(word) > width_of_window:
+#         lines.append(current)
+#         current = word
+#     else:
+#         current += " " + word
+#
+# lines.append(current)
+# print(lines)
+#
+
+
+# a=["abcd", "efgh", "hijk", "lmno", "pqrs", "tuvw", "xyz", "efgh", "hijk", "lmno", "pqrs", "abcd", "efgh", "hijk", "lmno", "pqrs", "tuvw", "xyz", "efgh", "hijk", "lmno", "pqrs", "abcd", "efgh", "hijk", "lmno", "pqrs", "tuvw", "xyz", "efgh", "hijk", "lmno", "pqrs"]
+#
+# my_dict = {i: a.count(i) for i in a}
+#
+# print(my_dict)
+#
